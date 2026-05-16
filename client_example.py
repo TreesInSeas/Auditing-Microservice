3c69a43b0279d1835ce27a69aec47ac8c7e32d9e
axlw6070 <zhanting@oregonstate.edu>
2026-05-17 06:21:10 +0800

    Update client_example.py

--- client_example.py
+++ client_example.py
@@ -1,3 +1,10 @@
 import requests
 BASE_URL = "http://127.0.0.1:1234"
 
+def add_to_audit_log(email, text):
+    response = requests.post(
+        f"{BASE_URL}/audit",
+        json={"email": email, "text": text}
+    )
+    print("Status code:", response.status_code)
+    print(response.json())
