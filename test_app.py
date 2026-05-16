9f20843574956a6abc15973cf2fb054ccbd552df
axlw6070 <zhanting@oregonstate.edu>
2026-05-17 06:30:06 +0800

    Update test_app.py

--- test_app.py
+++ test_app.py
@@ -2,3 +2,10 @@ import json
 import pytest
 import app as audit_app
 
+@pytest.fixture
+def client(tmp_path, monkeypatch):
+    test_log_file = tmp_path / "test_audit_log.json"
+    monkeypatch.setattr(audit_app, "LOG_FILE", test_log_file)
+    audit_app.app.config["TESTING"] = True
+    with audit_app.app.test_client() as client:
+        yield client
